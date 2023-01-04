c318d3fb89a6749aebf1676b8527b5b570081a1e
Jailson Paiva <j.psherl1009@gmail.com>
2023-01-04 19:26:07 -0400

    :feat added about section

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import styles from './About.module.scss';
+
+export function About() {
+    return (
+        <section className={styles.aboutContainer}>
+            <div>
+                <h3>Oi eu sou o <abbr title="Goku ㊗">Jailson✌</abbr>.</h3>
+                <p>Formado em Análise e Desenvolvimento de Sistemas e cursando pós-graduação em Engenharia de Software na PUC Minas venho me desenvolvendo como profissional e aluno buscando ser um grande Desenvolvedor que possa contribuir com o meu trabalho.</p>
+            </div>
+
+            <img src="https://github.com/JailsonPaiva.png" alt="Foto de perfil" />
+        </section>
+    );
+}
