ec1743a64b6bce121378d328a29be3444ce50f65
jailson paiva <jailsonp437@gmail.com>
2023-10-29 15:47:11 -0400

    style: add animation in section home and about

--- src/About.jsx
+++ src/About.jsx
@@ -1,30 +1,33 @@
-import { FaHtml5, FaCss3Alt, FaNodeJs, FaGithubSquare } from 'react-icons/fa' 
-import { SiJavascript, SiMysql } from "react-icons/si"
+import { FaHtml5, FaNodeJs, FaReact, FaGitAlt, FaNode } from 'react-icons/fa'
 import { IoLogoJavascript } from "react-icons/io";
 import { DiMysql } from "react-icons/di";
 
-
 import styles from './About.module.scss';
+import { useSpring, animated } from '@react-spring/web'
+
+import avatar from './assets/avatar-345.svg'
 
 export function About() {
+    const fadeIn = useSpring(
+        {
+            from: { opacity: 0 },
+            to: { opacity: 1 },
+            config: { duration: 2000, ease: [0.43, 0.13, 0.23, 0.96] },
+        }
+    );
+
     return (
-        <section className={styles.aboutContainer} id='about'>
+        <animated.section className={styles.aboutContainer} id='about' style={fadeIn}>
             <div>
                 <h3>Oi eu sou o <abbr title="Goku ㊗">Jailson✌</abbr>;</h3>
                 <p>Formado em Análise e Desenvolvimento de Sistemas apoixonado por tecnologia. Durante dois anos venho me desenvolvendo como profissional e aluno, buscando ser um grande Desenvolvedor onde eu possa contribuir com o meu trabalho.</p>
 
                 <div className={styles.skillContainer}>
-                    <h4>Soft Skills</h4>
+                    <h4>Hard Skills</h4>
 
                     <ul>
-                        <li> 
-                            <abbr title="HTML5"><FaHtml5 /></abbr>
-                        </li>
-                        <li> 
-                            <abbr title="CSS3"><FaCss3Alt /></abbr>
-                        </li>
                         <li>
-                            <abbr title="JavaScript"> <IoLogoJavascript /></abbr>
+                            <abbr title="React.JS"><FaReact /></abbr>
                         </li>
                         <li>
                             <abbr title="Node.JS"><FaNodeJs /></abbr>
@@ -33,16 +36,16 @@ export function About() {
                             <abbr title="MySQL"> <DiMysql /> </abbr>
                         </li>
                         <li>
-                            <abbr title="GIT"><FaGithubSquare /></abbr>
+                            <abbr title="Git"><FaGitAlt /></abbr>
                         </li>
                     </ul>
-                
+
                 </div>
             </div>
 
-            
 
-            <img src="https://github.com/JailsonPaiva.png" alt="Foto de perfil" />
-        </section>
+
+            <img src={avatar} alt="Foto de perfil" />
+        </animated.section>
     );
 } 
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,54 +3,76 @@ import React, { useState } from 'react';
 import Jimg from '../src/assets/J.svg'
 
 import styles from './Home.module.scss'
+import { useSpring, animated } from '@react-spring/web'
+
 
 export default function App() {
   const [selectedButton, setSelectedButton] = useState('home');
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   const handleButtonClick = (button) => {
-      setSelectedButton(button);
-      const section = document.getElementById(button);
-      section.scrollIntoView({behavior: 'smooth'});
+    setSelectedButton(button);
+    const section = document.getElementById(button);
+    section.scrollIntoView({ behavior: 'smooth' });
   }
+
+  const fadeIn = useSpring(
+    {
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+      config: { duration: 1500, ease: [0.43, 0.13, 0.23, 0.96] },
+    }
+  );
 
   return (
     <>
-      <header className={styles.headerContainer} >
+      <animated.header className={styles.headerContainer} style={fadeIn}>
         <section >
           <div>
             <span>J</span>veloper
           </div>
-          <ul className={styles.ulContainer}>
-            <li>
-              <button 
-              onClick={() => handleButtonClick('home')} 
-              className={`${selectedButton === 'home' ? styles.Active : ''}`}>HOME</button>
-            </li>
-            <li>
-            <button 
-              onClick={() => handleButtonClick('about')} 
-              className={`${selectedButton === 'about' ? styles.Active : ''}`}>SOBRE</button>
-            </li>
-            <li>
-            <button 
-              onClick={() => handleButtonClick('projects')} 
-              className={`${selectedButton === 'projects' ? styles.Active : ''}`}>PROJETOS</button>
-            </li>
-            <li>
-            <button 
-              onClick={() => handleButtonClick('contact')} 
-              className={`${selectedButton === 'contact' ? styles.Active : ''}`}>CONTATO</button>
-            </li>
-          </ul>
+
+          <nav>
+            <button
+              className={styles.navbarToggle}
+              onClick={() => setNavbarOpen((prev) => !prev)}
+            >
+              {navbarOpen ? 'close' : 'open'}
+            </button>
+
+
+            <ul className={styles.ulContainer}>
+              <li>
+                <button
+                  onClick={() => handleButtonClick('home')}
+                  className={`${selectedButton === 'home' ? styles.Active : ''}`}>HOME</button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleButtonClick('about')}
+                  className={`${selectedButton === 'about' ? styles.Active : ''}`}>SOBRE</button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleButtonClick('projects')}
+                  className={`${selectedButton === 'projects' ? styles.Active : ''}`}>PROJETOS</button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleButtonClick('contact')}
+                  className={`${selectedButton === 'contact' ? styles.Active : ''}`}>CONTATO</button>
+              </li>
+            </ul>
+          </nav>
         </section>
-      </header>
+      </animated.header>
 
 
-      <section className={styles.sectionContainer} id='home'>
+      <animated.section className={styles.sectionContainer} id='home' style={fadeIn}>
         <div>
-          <p>OI, <br /> 
-          EU SOU JAILSON, <br />
-          DESENVOLVEDOR WEB.</p>
+          <p>OI, <br />
+            EU SOU JAILSON, <br />
+            DESENVOLVEDOR WEB.</p>
           <span>Front-End | Back-end | Freelance</span>
 
           {/* <button>Entre em contato</button> */}
@@ -59,8 +81,8 @@ export default function App() {
         </div>
 
         <img src={Jimg} alt="Letra Jota" />
-      </section>
+      </animated.section>
     </>
-    )
+  )
 }
 
